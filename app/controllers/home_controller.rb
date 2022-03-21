37f2f63211495d3276564b0d90915971b4356ba8
Aji Kesuma <ajikesuma25@gmail.com>
2022-03-21 16:18:28 +0700

    add new pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def couple
+  end
+
+  def location
+  end
+
+  def wishes
+  end
+
+  def gift
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/couple'
+  get 'home/location'
+  get 'home/wishes'
+  get 'home/gift'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
