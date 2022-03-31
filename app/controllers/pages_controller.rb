c1061f30fba18089360af3bbf765383174d40fa7
supermylo <sightraster@gmail.com>
2022-03-31 13:42:30 +0700

    create pages update progress

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+    def couples
+    end
+
+    def goblog
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'home/location'
   get 'home/wishes'
   get 'home/gift'
+  get 'pages/couples'
+  get 'pages/goblog'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
