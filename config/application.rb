b45392b4d40f65b70b5a55f17c91ee4454b428d0
Aji Kesuma <ajikesuma25@gmail.com>
2022-04-11 16:21:15 +0700

    Add carousel to section conten in location page sixth template

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mengundangmu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "replacing-cruise-conflicts-semester.trycloudflare.com"
+    config.hosts << "platforms-respiratory-leave-ii.trycloudflare.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
