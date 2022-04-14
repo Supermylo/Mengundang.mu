919d262ebb9a1ad611b56e0f2c2d41514163de4a
Aji Kesuma <ajikesuma25@gmail.com>
2022-04-14 15:00:35 +0700

    Move all sixthtemplate stylesheet to folder stylesheet/sixthtemplate and link it to the html files

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mengundangmu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "platforms-respiratory-leave-ii.trycloudflare.com"
+    config.hosts << "cloth-window-lifestyle-bids.trycloudflare.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
