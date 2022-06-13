0cdf5942c2982121c692dd1dd791963970e0c21f
Aji Kesuma <ajikesuma25@gmail.com>
2022-06-13 10:13:19 +0700

    Styling section showcase - mobile

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mengundangmu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "indirect-pas-chest-aa.trycloudflare.com"
+    config.hosts << "om-patent-purchasing-blues.trycloudflare.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
