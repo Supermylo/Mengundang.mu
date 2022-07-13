e73ac8cfb7fd2ca5f8511df1fdf6204414788aeb
Aji Kesuma <ajikesuma25@gmail.com>
2022-07-13 14:10:52 +0700

    Add typing animation to sub-headline in header section

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
   get 'sixthtemplate/gift'
 
   get 'landingpage/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
