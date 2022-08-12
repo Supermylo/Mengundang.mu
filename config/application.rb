161965eae04057003370dd75c56ab79877933e02
Aji Kesuma <ajikesuma25@gmail.com>
2022-08-12 11:26:36 +0700

    Add animation to all page in FirstTemplate and change the main font

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mengundangmu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "essays-nationally-broadcasting-strain.trycloudflare.com"
+    config.hosts << "road-meal-doing-burn.trycloudflare.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
