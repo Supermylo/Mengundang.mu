344e756205033928ddfd0094f74cba6d1b12fbde
Aji Kesuma <ajikesuma25@gmail.com>
2022-09-16 14:53:34 +0700

    Add animation to homepage & couple page in Sixth Template

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mengundangmu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "read-kirk-nylon-present.trycloudflare.com"
+    config.hosts << "ex-mh-heroes-scout.trycloudflare.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
