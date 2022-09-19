6085ab8a85b3bffb37c0040f64025047ee0e15a2
Aji Kesuma <ajikesuma25@gmail.com>
2022-09-19 10:00:53 +0700

    Add animation to Wishes and Gift Page in Sixth Template

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mengundangmu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "ex-mh-heroes-scout.trycloudflare.com"
+    config.hosts << "baker-difficulty-village-horizontal.trycloudflare.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
